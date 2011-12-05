1af570698b5164ea67fd7dbe8878ec9f452d1710
mje <mirek@squiz.pl>
2011-12-05 14:37:57 +0000

    Update jquery/jquery.funnelbackplus.js

--- jquery/jquery.funnelbackplus.js
+++ jquery/jquery.funnelbackplus.js
@@ -1,15 +1,15 @@
 var Funnelback = {
-			
+
     init: function (options, elem) {
     	this.options = jQuery.extend({},this.options,options);
     	this.formatters = jQuery.extend({},this.formatters,this.options.formatters);
 		this.elem = elem;
 		this.$elem = jQuery(elem);
-		
+
 		this._build();
 		return this;
 	},
-	
+
 	options: {
 		url : '/s/search.html',
 		form : 'simple',
@@ -22,11 +22,11 @@ var Funnelback = {
     },
     
     targets: {}, // targets is a list of objects in the format { 'path': <jquery elem>, 'formatter': <formatter name> } 
-	
+
     _build: function(){
 		this.jsonpbind();
     },
-	
+
 	jsonpbind: function() {
 		var fbbase = this;
     	jQuery("a." + this.options.captureClass).live('click', function(e) {
@@ -37,7 +37,7 @@ var Funnelback = {
     		});
     	});		
 	},
-	
+
 	jsonpsearch: function(query){
 		var fbbase = this;
 		var params = {
@@ -50,11 +50,11 @@ var Funnelback = {
 			fbbase.jsonprefresh(url, this['path'], fbbase.formatters[this['formatter']], fbbase.options.fbloader);
 		});
 	},
-	
+
 	jsonprefresh: function(url, path, formatter, fbloader){
 		// show loader with timeout 
-		// var showLoader;
-		// showLoader = setTimeout("$('#loader').show()", 300);
+		var showLoader;
+		showLoader = setTimeout("$('#loader').show()", 300);
 		if (fbloader) { 
 			fbloader.show();
 		};
@@ -64,28 +64,50 @@ var Funnelback = {
 			dataType: 'jsonp',
 			success: function(json) {
 				path.html(formatter(json));
-				// clearTimeout(showLoader);
-		        if (fbloader) {
+				clearTimeout(showLoader);
+		        	if (fbloader) {
 					fbloader.hide();		        	
-		        }
+		        	}
 			}
 		});
 	},
-	
+
 	formatters: {
 		'searchResultsList': function(json){
 			var rendered = '<ul>';
 			jQuery.each(json.response.resultPacket.results, function(){
 				var result = this;
 				rendered += '<li>';
-				rendered += '<a href="' + result.clickTrackingUrl + '">';
-				rendered += result.title + '</a></li>';
+				rendered += '<a class="fbClickUrl" href="' + result.clickTrackingUrl + '">' + result.title + '</a>';
+				rendered += '<p class="fbSummary">' + result.summary + '</p>';
+				rendered += '<p class="fbDisplayUrl">' + result.displayUrl + '</p>';
+				rendered += '</li>';
 			});
 			rendered += '</ul>';
-			var baselink = '?' + json.QueryString.replace(/&callback=jsonp\d+/, "");
+			var baselink = '?' + json.QueryString.replace(/&callback=.+/, "");
 			baselink = baselink.replace(/&start_rank=\d+/, '');
 			pagelink = baselink + "&start_rank=" + json.response.resultPacket.resultsSummary.nextStart;
-			rendered += '<p><a href="' + pagelink + '" class="fbcapture">More</a></p>';
+			// rendered += '<p><a href="' + pagelink + '" class="fbcapture">More</a></p>';
+
+			var prevPage = Math.round(json.response.resultPacket.resultsSummary.currStart - json.response.resultPacket.resultsSummary.numRanks);
+			var nextPage = Math.round(json.response.resultPacket.resultsSummary.currStart + json.response.resultPacket.resultsSummary.numRanks);
+			var pageClassPrev = '';
+			var pageClassNext = '';
+			var pageClassNum = '';
+			if (prevPage < 0) { pageClassPrev = 'hidden'; }
+			if (nextPage > json.response.resultPacket.resultsSummary.totalMatching) { pageClassNext = 'hidden'; }
+			var numOfPages = Math.ceil(json.response.resultPacket.resultsSummary.totalMatching / json.response.resultPacket.resultsSummary.numRanks);
+                        var pagesArray = new Array(numOfPages);
+                        var paginationString = '';
+                        for(var i=0; i<pagesArray.length; i++){
+                                pagesArray[i] = '<a class="fbcapture" href="' + baselink + '&start_rank=' + i+1 + '">' + Math.round(i+1) + '</a>';
+                        }
+                        jQuery.each(pagesArray, function(index, value) {
+                                paginationString += value + ' ';
+			});
+			
+			rendered += '<p class="pageControls"><a class="fbcapture ' + pageClassPrev + '" href="' + baselink + '&start_rank=' + prevPage + '">Prev </a>' + paginationString + '<a class="fbcapture ' + pageClassNext +'" href="' + baselink + '&start_rank=' + nextPage + '"> Next</a></p>';
+			
 			return rendered;
 		},
 		'targetClasslist': function(thisfunnelback) {
@@ -94,6 +116,14 @@ var Funnelback = {
 				classlist += this.captureClass + ' ';
 			});
 			return classlist;
+		},
+                'searchResultsCount': function(json) {
+			var resultsCount = 'Displaying <span class="orange">' + json.response.resultPacket.resultsSummary.currStart + ' </span>to <span class="orange">' + json.response.resultPacket.resultsSummary.currEnd + ' </span>of <span class="orange">' + json.response.resultPacket.resultsSummary.totalMatching + '</span> results.';
+                        return resultsCount;
+                },
+		'currentSearchQuery': function(json) {
+			var searchQueryTerm = json.response.resultPacket.query;
+			return searchQueryTerm;
 		},
 		'contextualNavigationList': function(json){
 			var rendered = '<ul>';
@@ -115,7 +145,7 @@ var Funnelback = {
 			return rendered;
 		},
 	},
-	
+
 };
 
 if (typeof Object.create !== 'function') {
